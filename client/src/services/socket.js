18cfab2bc89cd50a76920ecbf18731db7a139dbd
Jai Gobind Pal Singh <jai.gobind.pal@gmail.com>
2026-04-12 13:38:25 +0530

    changed socket adress for deployment

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Replace with your backend server URL
-const URL = 'http://localhost:3000'; 
+const URL = 'https://sixsem-project-zxlt.onrender.com/'; 
 
 export const socket = io(URL, {
   autoConnect: false // Set to false if you want to manually connect later
